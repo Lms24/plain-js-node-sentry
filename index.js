2948952a9db6018b94a570ee54d289f9c9ffed83
Lukas Stracke <lukas.stracke@sentry.io>
2024-07-24 11:27:32 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require("./instrument");
+
+const Sentry = require("@sentry/node");
+
+// Sentry.startSpan(
+//   {
+//     op: "test",
+//     name: "My First Test Span",
+//   },
+//   () => {
+try {
+  foo();
+} catch (e) {
+  Sentry.captureException(e);
+}
+//   }
+// );
---
+++ instrument.js
@@ -0,0 +1,19 @@
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+const Sentry = require("@sentry/node");
+const { nodeProfilingIntegration } = require("@sentry/profiling-node");
+
+Sentry.init({
+  dsn: "https://0d1038ca38c71c2c43f713d0c2aa58c5@o447951.ingest.us.sentry.io/4507655858946048",
+  integrations: [nodeProfilingIntegration()],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+
+  beforeSend(event) {
+    console.log("Event captured", event);
+    // Modify the event here
+    return event;
+  },
+});
